d738584a5be945d6fced8d383470ddb7af9f7b65
Wakeel03 <wbauboorally@gmail.com>
2021-03-31 12:13:32 +0400

    + 'add to cart' functionality for carousel items

--- src/components/CarouselItem/CarouselItem.jsx
+++ src/components/CarouselItem/CarouselItem.jsx
@@ -1,15 +1,24 @@
 import './CarouselItem.css'
 
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
-function CarouselItem({carouselItem, data}) {
+import { addToCart } from '../../redux/cartItemSlice'
+
+function CarouselItem({id, carouselItem, data}) {
+    const dispatch = useDispatch()
+
+    const addItemToCart = () => {
+        dispatch(addToCart(id))
+    }
+
     return (
         <div className={carouselItem}>
             <div className="carouselItem__itemInfo">
                 <h2>{data.name}</h2>
                 <p>{data.description}</p>
                 <h1>$ {data.price}</h1>
-                <button>Shop Now</button>
+                <button onClick={addItemToCart}>Shop Now</button>
             </div>
             <img src={data.image} alt="" className="carouselItem__itemImg" />
         </div>
